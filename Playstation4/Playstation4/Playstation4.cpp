0313fa12bd34547cbba3bbfb331ac6a17436451a
Probook14 <Probook14@192.168.1.134>
2017-01-23 13:58:23 -0300

    Inicial

---
+++ Playstation4/Playstation4/Playstation4.cpp
@@ -0,0 +1,10 @@
+#include "Playstation4.h"
+
+Playstation4::Playstation4()
+{
+}
+
+Playstation4::~Playstation4()
+{
+}
+
---
+++ Playstation4/Playstation4/Playstation4.h
@@ -0,0 +1,12 @@
+#ifndef PLAYSTATION4_H
+#define PLAYSTATION4_H
+
+class Playstation4
+{
+public:
+    Playstation4();
+    ~Playstation4();
+
+};
+
+#endif // PLAYSTATION4_H
