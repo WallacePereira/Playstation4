c9d2d81c646278917899eae277302b934146d7dd
FACOMP <FACOMP@LABM-PC07>
2017-02-06 18:12:33 -0300

    ok

--- Playstation4/Playstation4/main.cpp
+++ Playstation4/Playstation4/main.cpp
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include "Data.h"
 #include "Playstation4.h"
-
+Playstation4 ps4;
 int main(int argc, char **argv)
 {
-	Playstation4 Ps4;
-
+	ps4.getGenero();
+    ps4.getIdade();
+    ps4.getNome();
+    ps4.setDados();
+    ps4.setDataNascimento();
+    ps4.setGenero();
+    ps4.setIdade();
+    ps4.setNome();
 	return 0;
 }
